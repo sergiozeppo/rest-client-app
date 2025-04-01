3a1524e130aaf2c7728639b0ee875c58fa99631b
yelantsevv <yelantsevv@gmail.com>
2025-04-01 21:03:59 +0300

    fix:Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
 import { useTranslations } from 'next-intl';
 import { LocaleSwitcher } from '@/components';
-import s from './Header.module.scss';
+import styles from './Header.module.scss';
 
 export default function Header() {
   const t = useTranslations('Header');
   return (
-    <div className={s.title}>
-      <h2>Logo</h2>
+    <div className={styles.title}>
+      <img
+        src="https://raw.githubusercontent.com/rolling-scopes-school/tasks/refs/heads/master/react/assets/rss-logo.svg"
+        alt="Course Logo"
+        className={styles.logo}
+      />
       <div>
         <LocaleSwitcher />
       </div>
       <div>
-        <button className={s.btn}>{t('Sign_In')}</button>
-        <button className={s.btn}>{t('Sign_up')}</button>
+        <button className={styles.btn}>{t('Sign_In')}</button>
+        <button className={styles.btn}>{t('Sign_up')}</button>
       </div>
     </div>
   );
