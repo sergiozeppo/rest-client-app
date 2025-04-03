2ddadc54c4189df4ea26720677c7d018231945a1
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-04-03 19:34:22 +0500

    feat: implement sticky header logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,29 @@
+'use client';
+
 import { useTranslations } from 'next-intl';
 import { LocaleSwitcher, ThemeSwitcher } from '@/components';
 import styles from './Header.module.scss';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleStickyHeader = () => {
+      setIsSticky(window.scrollY > 20);
+    };
+
+    window.addEventListener('scroll', handleStickyHeader);
+    return () => window.removeEventListener('scroll', handleStickyHeader);
+  }, []);
+
   const t = useTranslations('Header');
   return (
-    <div className={styles.container}>
+    <div
+      className={
+        isSticky ? `${styles.container} ${styles.sticky}` : styles.container
+      }
+    >
       <img
         src="https://raw.githubusercontent.com/rolling-scopes-school/tasks/refs/heads/master/react/assets/rss-logo.svg"
         alt="Course Logo"
