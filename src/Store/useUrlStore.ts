150ef4fe85874dc299cf392865d2bfed2982d976
yelantsevv <yelantsevv@gmail.com>
2025-04-04 16:45:08 +0300

    add: handleInput

--- src/Store/useUrlStore.ts
+++ src/Store/useUrlStore.ts
@@ -3,8 +3,8 @@ import { devtools } from 'zustand/middleware';
 
 export type Params = {
   locale?: string;
-  method?: string;
-  url?: string;
+  method: string;
+  url: string;
 };
 type Query = Record<string, string>;
 
@@ -12,6 +12,9 @@ type Url = {
   method: string;
   locale: string;
   urlBase: string;
+  queryBase: string;
+  valueBase: string;
+  value: string;
   params: Params;
   query: Query;
   // eslint-disable-next-line
@@ -26,17 +29,34 @@ export const useUrl = create<Url>()(
       method: '',
       locale: '',
       urlBase: '',
+      queryBase: '',
+      valueBase: '',
       params: {},
       query: {},
       setParams: (params) => {
+        const url = params?.url?.split('%')[0] || '';
+        const valueBase = decodeURIComponent(atob(url));
         set((state) => ({
           locale: params.locale || 'en',
           params: params || {},
-          method: params?.method.toUpperCase() || 'GET',
-          urlBase: `/${params?.method || state.method}/${params.url || ''}`,
+          method: params?.method.toUpperCase() || 'get',
+          urlBase: `/${params?.method || state.method}${params.url || ''}`,
+          valueBase,
+          value: valueBase + state.queryBase,
         }));
       },
-      setQuery: (query) => set({ query }),
+      setQuery: (query) => {
+        const queryBase =
+          Object.keys(query).length > 0
+            ? `?${new URLSearchParams(query).toString()}`
+            : '';
+
+        set((state) => ({
+          query,
+          queryBase: queryBase,
+          value: state.valueBase + queryBase,
+        }));
+      },
     }),
     { name: 'useUrl' }
   )
--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,4 +1,47 @@
+'use client';
+import { ChangeEvent, useTransition, KeyboardEvent } from 'react';
 import styles from './SearchInput.module.scss';
+import { useUrl } from '@/Store/useUrlStore';
+import { useRouter } from '@/i18n/navigation';
 export default function SearchInput() {
-  return <input className={styles.input} type="text" name="search" />;
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const { params, value } = useUrl();
+
+  function handleInput(e: ChangeEvent<HTMLInputElement>) {
+    const url = e.target.value.trim();
+    if (!url || url === 'http://' || url === 'https://') return;
+    const cleanUrl = url.match(/^(http|https):\/\//) ? url : `https://${url}`;
+    const { origin, pathname, search } = new URL(cleanUrl);
+    const base64 = btoa(encodeURIComponent(origin + pathname));
+    router.replace({
+      pathname: `/${params.method || 'get'}/${base64}${search}`,
+    });
+  }
+
+  function handleEnter(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.code === 'Enter') {
+      startTransition(async () => {
+        try {
+          const data = await fetch(value);
+          const json = await data?.json();
+          console.log(json);
+        } catch (error) {
+          console.log('error in fetch', error);
+        }
+      });
+    }
+  }
+
+  return (
+    <input
+      disabled={isPending}
+      defaultValue={value}
+      onChange={handleInput}
+      onKeyDown={handleEnter}
+      className={styles.input}
+      type="text"
+      name="search"
+    />
+  );
 }
