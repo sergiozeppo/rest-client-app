66299348e17d210a891773e222c26983c280c4d8
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-04-06 00:32:35 +0500

    fix: add redirection on main page on click

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,17 +3,20 @@
 import { useTheme } from '@/Store/Theme';
 import Image from 'next/image';
 import styles from './Logo.module.scss';
+import Link from 'next/link';
 
 export default function Logo() {
   const { theme } = useTheme();
 
   return (
-    <Image
-      src={theme === 'light' ? '/logos/NR_light.PNG' : '/logos/NR_dark.PNG'}
-      alt="NeverREST Logo"
-      width="60"
-      height="60"
-      className={styles.logo}
-    />
+    <Link href="/">
+      <Image
+        src={theme === 'light' ? '/logos/NR_light.PNG' : '/logos/NR_dark.PNG'}
+        alt="NeverREST Logo"
+        width="60"
+        height="60"
+        className={styles.logo}
+      />
+    </Link>
   );
 }
