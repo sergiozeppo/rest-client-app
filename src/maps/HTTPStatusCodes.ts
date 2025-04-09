14dc8bef291c8c68ae533096909f793054a615e8
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-04-09 22:08:23 +0500

    feat: add HTTP codemap and getStatusText function

---
+++ src/maps/HTTPStatusCodes.ts
@@ -0,0 +1,17 @@
+export const statusTextMap = new Map<number, string>([
+  [200, 'OK'],
+  [201, 'Created'],
+  [204, 'No Content'],
+  [400, 'Bad Request'],
+  [401, 'Unauthorized'],
+  [403, 'Forbidden'],
+  [404, 'Not Found'],
+  [405, 'Method Not Allowed'],
+  [408, 'Request Timeout'],
+  [409, 'Conflict'],
+  [429, 'Too Many Requests'],
+  [500, 'Internal Server Error'],
+  [502, 'Bad Gateway'],
+  [503, 'Service Unavailable'],
+  [504, 'Gateway Timeout'],
+]);
