3c99e5d6d873fbde06d6e238ccf1dd1bf27f8db2
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-04-09 21:55:12 +0500

    feat: add time and size colors

--- src/utils/getColoringStatus/getColoringStatus.ts
+++ src/utils/getColoringStatus/getColoringStatus.ts
@@ -1,9 +1,21 @@
 interface ColoringProps {
   status: number;
+  time: number;
+  size: string;
 }
 
-export default function getColoringStatus({ status }: ColoringProps) {
+export default function getColoringStatus({
+  status,
+  time,
+  size,
+}: ColoringProps) {
   const statusColor = status >= 200 && status < 300 ? 'green' : 'red';
+  const timeColor = time <= 200 ? 'green' : time <= 1000 ? 'yellow' : 'red';
+  const sizeColor = +size <= 100 ? 'green' : +size <= 500 ? 'yellow' : 'red';
 
-  return statusColor;
+  return {
+    statusColor,
+    timeColor,
+    sizeColor,
+  };
 }
