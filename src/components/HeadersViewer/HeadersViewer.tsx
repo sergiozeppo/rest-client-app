1b0394d9e4522313a3407c73e5596055a4db19f2
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-04-16 21:01:09 +0500

    fix: fix the headersview

--- src/components/HeadersViewer/HeadersViewer.tsx
+++ src/components/HeadersViewer/HeadersViewer.tsx
@@ -8,31 +8,38 @@ export default function HeadersViewer() {
   const error = useFetch((state) => state.error);
   const data = error || headers;
 
-  if (!data)
+  if (!data) {
     return (
       <div className={styles['hdrs-viewer']}>
         No headers yet. Try to get some data!
       </div>
     );
+  }
 
-  const json = typeof data === 'string' ? data : JSON.stringify(data, null, 2);
-  const lines = json.split('\n');
-  const cleanedLines = lines.map((line) => line.replace(/\\/g, ''));
+  let parsed: Record<string, string> = {};
+
+  const raw =
+    typeof data === 'string' ? data.replace(/\\/g, '') : JSON.stringify(data);
+  parsed = JSON.parse(raw);
 
   return (
     <div className={styles['hdrs-viewer']}>
-      {json && (
-        <div className={styles['hdrs-copy-wrapper']}>
-          <button className={styles['hdrs-copy-btn']}></button>
+      <div className={styles['hdrs-table']}>
+        <div className={styles['hdrs-row'] + ' ' + styles['hdrs-header']}>
+          <div className={styles['hdrs-cell']}>Header</div>
+          <div className={styles['hdrs-cell']}>Value</div>
         </div>
-      )}
-      <pre className={styles['hdrs-code-block']}>
-        {cleanedLines.map((line, i) => (
-          <div key={i} className={styles['hdrs-code-line']}>
-            <span className={styles['hdrs-line-text']}>{line}</span>
+        {Object.entries(parsed).map(([key, value], i) => (
+          <div key={i} className={styles['hdrs-row']}>
+            <div className={styles['hdrs-cell']}>{key}</div>
+            <div className={styles['hdrs-cell']}>
+              {typeof value === 'object'
+                ? JSON.stringify(value, null, 2).replace(/\\/g, '')
+                : value?.toString().replace(/\\/g, '')}
+            </div>
           </div>
         ))}
-      </pre>
+      </div>
     </div>
   );
 }
