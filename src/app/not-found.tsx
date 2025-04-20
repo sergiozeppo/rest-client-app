756410f9e223eaf6493649d99579d19cd4b67031
yelantsevv <yelantsevv@gmail.com>
2025-04-20 13:05:16 +0300

    fix: test errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,5 @@
 import { NotFound404 } from '@/components';
 
 export default function NotFound() {
-  return (
-    <html>
-      <body>
-        <NotFound404 />
-      </body>
-    </html>
-  );
+  return <NotFound404 />;
 }
